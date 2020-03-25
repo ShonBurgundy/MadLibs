449d67b909407796477f892e6c03b68466810f49
Dominic Montelongo <montelongodominic@gmail.com>
2020-03-25 09:52:44 -0700

    Explore fundamentals from previous day and practice debuggin with Michael's js code

--- js/scripts.js
+++ js/scripts.js
@@ -20,24 +20,40 @@
 //   });
 // });
 
-$(document).ready(function() {
-  $("#formOne").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+// $(document).ready(function() {
+//   $("#formOne").submit(function(event) {
+//     var person1Input = $("input#person1").val();
+//     var person2Input = $("input#person2").val();
+//     var animalInput= $("input#animal").val();
+//     var exclamationInput = $("input#exclamation").val();
+//     var verbInput = $("input#verb").val();
+//     var nounInput = $("input#noun").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+//     $(".person1").text(person1Input);
+//     $(".person2").text(person2Input);
+//     $(".animal").text(animalInput);
+//     $(".exclamation").text(exclamationInput);
+//     $(".verb").text(verbInput);
+//     $(".noun").text(nounInput);
+
+//     $("#story").show();
+
+//     event.preventDefault();
+    
+//   });
+// });
+
+$(document).ready(function() {
+  $("#blanks form").submit(function() {
+    alert('Got to beginning of form submit!');
+    var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+
+    blanks.forEach(function(blank) {
+      var userInput = $("input." + blank).val();
+      $("." + blank).text(userInput).val();      
+    });
 
     $("#story").show();
-
-    event.preventDefault();
+    alert('Got to end of form submit!');
   });
 });
